ec543be606d8dc1ca264c68931e1c78b0bf7928f
rocketspops <rocketspops@gmail.com>
2013-02-26 10:57:34 -0600

    add brief comments to surface.js

--- app/assets/javascripts/surface.js
+++ app/assets/javascripts/surface.js
@@ -1,7 +1,11 @@
 //= require hashgrid 
 
 $(function() {
+
+  // Focus first form field with an error on it.
   $(".m-form__field--error").first().children(".m-form__input").focus();
+
+  // Fade out notice after 10 seconds.
   $(".m-notice--confirm, .m-notice--notice").delay(10000).fadeOut("slow");
  });
 
