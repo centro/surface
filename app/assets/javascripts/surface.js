1c11eb4feb4b1f9c8af894dde7c231c4360ffe08
rocketspops <rocketspops@gmail.com>
2013-02-26 16:53:38 -0600

    add password masking toggle

--- app/assets/javascripts/surface.js
+++ app/assets/javascripts/surface.js
@@ -7,5 +7,41 @@ $(function() {
 
   // Fade out notice after 10 seconds.
   $(".m-notice--confirm, .m-notice--notice").delay(10000).fadeOut("slow");
+
+  // Function calls.
+  
+  unmaskPassword();
+
  });
+
+// Toggle masked/unmasked password fields. 
+
+var unmaskPassword = function () {
+
+  $('.m-form__input--password').attr('type', 'text');
+  $('.js-masking-toggle').text("Hide")
+
+  $('.js-masking-toggle').click(function() {
+
+    var input = $(this).prev('input');
+    var inputType = $(this).prev('input').attr('type');
+
+    if (inputType === 'text') {
+      input.attr('type', 'password');
+    } else {
+      input.attr('type', 'text');
+    }
+
+    // Change link text.
+
+    var linkText = $('.js-masking-toggle').text();
+
+    if (linkText === 'Hide') {
+      $('.js-masking-toggle').text("Show");
+    } else {
+      $('.js-masking-toggle').text("Hide");
+    }
+
+  }); 
+}
 
