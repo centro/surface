e21ba2420eb8c19ff25ddc66dd55550c69bd98f7
rocketspops <rocketspops@gmail.com>
2013-02-27 12:25:22 -0600

    update form field error message js and hover styles

--- app/assets/javascripts/surface.js
+++ app/assets/javascripts/surface.js
@@ -3,7 +3,7 @@
 $(function() {
 
   // Focus first form field with an error on it.
-  $(".m-form__field--error").first().children(".m-form__input").focus();
+  $(".m-form__field--error").first().children("input").focus();
 
   // Fade out notice after 10 seconds.
   $(".m-notice--confirm, .m-notice--notice").delay(10000).fadeOut("slow");
