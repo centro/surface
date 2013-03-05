7ba71da9bb04cbf01e1d6143bf76792be1da463c
Chris TenHarmsel <chris.tenharmsel@centro.net>
2013-03-05 14:16:08 -0600

    Adding field decorator helper

---
+++ app/helpers/surface/application_helper.rb
@@ -0,0 +1,14 @@
+module Surface
+  module ApplicationHelper
+    ## Field Decorator helper
+    ## This helper will wrap it's input block in a div.  That div will have it's class
+    ## set to to options[:class] or options[:class_error] depending on whether or not
+    ## instance.field has any validation errors.
+    def field_decorator(instance, field, options = {}, &block)
+      class_with_errors = options.delete(:class_error)
+      class_without_errors = options.delete(:class)
+      options[:class] = instance.object.errors[field].empty? ? class_without_errors : class_with_errors
+      concat render(:partial => "shared/field_decorator", :locals => {:block => block, :options => options})
+    end
+  end
+end
