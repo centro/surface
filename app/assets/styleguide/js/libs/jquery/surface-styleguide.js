66c559ee0c4bffa35aca464b3ca20e3ccfd97da1
rocketspops <rocketspops@gmail.com>
2013-05-31 14:23:51 -0500

    add a couple comments to the styleguide js

--- app/assets/styleguide/js/libs/jquery/surface-styleguide.js
+++ app/assets/styleguide/js/libs/jquery/surface-styleguide.js
@@ -1,7 +1,11 @@
 $(function() {
 
-  $('.m-form__field--error, .m-form__infield--error').first().children('input').focus();
+  // Automatically :focus the first field that has an error
+  
+  $('[class*="field--error"]').first().children('input').focus();
 
+  // Only fade infield label opacity if the browser supports it
+ 
   if (!$.support.opacity) {
     $('.m-form__label--infield').inFieldLabels({labelClass: '', fadeDuration: 100, fadeOpacity: 1});
   } else {
